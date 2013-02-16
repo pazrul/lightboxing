8ccf5f70c4b15c0ca6d543978520e7116c089ea9
Jeremy Whitbred <pazrul@gmail.com>
2013-02-16 11:10:25 -0800

    added sass file and js overlay

---
+++ js/lightboxing-ck.js
@@ -0,0 +1,1 @@
+var toOpen=[];for(img in document.images)console.log(img);$('a[data-boxing="true"]').click(function(e){$('<div id="xyzoverlay"></div>').appendTo("body");return!1});
--- js/lightboxing.js
+++ js/lightboxing.js
@@ -3,3 +3,9 @@ var toOpen = [];
 for (img in document.images){
 	console.log(img);
 };
+
+$('a[data-boxing="true"]').click(function(e){
+	$('<div id="xyzoverlay"></div>').appendTo('body');
+
+	return false;
+});
