489f70dc3fca44b84b0f84fed5e2b9ef8f03f607
Jeremy Whitbred <pazrul@gmail.com>
2013-02-21 12:23:19 -0800

    Clearup, added comments

--- js/lightboxing-ck.js
+++ js/lightboxing-ck.js
@@ -1,5 +1,2 @@
-//var toOpen = [];
-//for (img in document.images){
-//	console.log(img);
-//};
-function removeBoxing(){$("#xyzoverlay").fadeOut("700",function(){$(this).remove()})}$(document).ready(function(){$('a[data-boxing="true"]').click(function(e){e.preventDefault();var t=$(this).attr("href");$overlay=$('<div id="xyzoverlay"></div>');$overlay.hide().appendTo("body").fadeIn("900");$('<div id="xyzframe"/>').appendTo("#xyzoverlay").css({top:window.innerHeight/2,left:window.innerWidth/2});$('<img class="xyzimg">').attr("src",t).ready(function(){$("#xyzframe").animate({top:window.innerHeight/2-this.height/2,left:window.innerWidth/2-this.width/2},2e3)}).appendTo("#xyzframe");$overlay.click(function(e){removeBoxing()})})});
+//constants
+function removeBoxing(){$("#xyzoverlay").fadeOut("700",function(){$(this).remove()})}var KEY_ESC=27;$(document).ready(function(){$('a[data-boxing="true"]').click(function(e){e.preventDefault();var t=$(this).attr("href"),n=$('<div id="xyzoverlay"></div>');n.hide().appendTo("body").fadeIn("900");$('<div id="xyzframe"/>').appendTo("#xyzoverlay").css({top:window.innerHeight/2,left:window.innerWidth/2});$('<img class="xyzimg">').attr("src",t).ready(function(){$("#xyzframe").animate({top:window.innerHeight/2-this.height/2,left:window.innerWidth/2-this.width/2},2e3)}).appendTo("#xyzframe");n.click(function(e){removeBoxing()});$("#overlay").keyup(function(e){e.keyCode==KEY_ESC&&removeBoxing()})})});
--- js/lightboxing.js
+++ js/lightboxing.js
@@ -1,25 +1,42 @@
-//var toOpen = [];
+//constants
 
-//for (img in document.images){
-//	console.log(img);
-//};
+var KEY_ESC = 27;
 
+//helper functions
+
+function removeBoxing(){
+	$('#xyzoverlay')
+		.fadeOut('700', function(){
+			$(this).remove();
+	});
+}
+
+//Need an animate and center funciton. I don't want to do it how it is now
+
+
+
+// Start watching for clicks, and prevent clicking through
 $(document).ready(function(){
   $('a[data-boxing="true"]').click(function(e){
 		e.preventDefault();
+
+		//get the address for the large size image
 		var imgUrl = $(this).attr('href');
 		
-		$overlay = $('<div id="xyzoverlay"></div>');
-		$overlay
+		//start overlay creation. This can be cleaned up to have all of it in one declaration
+		var overlay = $('<div id="xyzoverlay"></div>');
+		overlay
 			.hide()
 			.appendTo('body')
 			.fadeIn('900');
 
+		//Image border and containment div
 		$('<div id="xyzframe"/>').appendTo('#xyzoverlay').css({
 			'top': (window.innerHeight / 2),
 			'left': (window.innerWidth / 2)
 		});
 
+		//Actual image load. Doesn't animate properly and it needs to be fixed.
 		$('<img class="xyzimg">').attr('src', imgUrl)
 			.ready(function(){
 				$('#xyzframe').animate({
@@ -30,17 +47,20 @@ $(document).ready(function(){
 
 			})
 			.appendTo('#xyzframe');
-			
-		$overlay.click(function(e){
+		
+		/*Need to change this to stop event propagation. 
+		If someone clicks on the image it currently removes the image and overlay.
+		Also want to add 'esc' key to leave.
+		*/
+		
+		overlay.click(function(e){
 			removeBoxing();
+		});
+
+		//should remove lightbox on esc. It doesn't.
+		$('#overlay').keyup(function(e) {
+			if (e.keyCode == KEY_ESC) {removeBoxing();}
 		});
   });
 
 });
-
-function removeBoxing(){
-	$('#xyzoverlay')
-		.fadeOut('700', function(){
-			$(this).remove();
-	});
-}
